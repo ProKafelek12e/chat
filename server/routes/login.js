e024fa25772b1f09a874155624ea401d354ace20
prokafelek12e <pro.kafelek@gmail.com>
2025-08-02 01:44:24 +0200

    hashed passwords

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const argon2 = require('argon2');
 
 const connectToDB = require('../lib/mongo');
 const generateToken = require('../utils/token');
@@ -7,19 +8,23 @@ const generateToken = require('../utils/token');
 
 router.post('/login', (req,res) => {
     const body = req.body
-    console.log(body)
     const se =async ()=>{
 
         const db = await connectToDB(); // get the db instance
-        const result = await db.collection('users').findOne({username:body.username,password:body.password});  // insert document
+        const result = await db.collection('users').findOne({username:body.username});  // insert document
         // username and password combination not found
         if(result===null){
             console.log('failed Login')
             res.status(401).json({'message':'username or password is incorrect'})
         }
+        const validPassword = await argon2.verify(result.password,body.password);
+        if(!validPassword){
+            console.log('failed password')
+            res.status(401).json({'message':'username or password is incorrect'})
+        }
         else{
             console.log('Login Sucesfull')
-            let token = await generateToken(client=body.client, username=body.username)
+            let token = await generateToken(body.client, body.username)
             res.status(200).json({'token': token})
         }
     }
@@ -50,14 +55,13 @@ router.post('/signup', async(req,res) => {
 
         try{
             const check = await db.collection('users').findOne({username:body.username})
-            console.log(check)
             if(check===null){
-
-            const result = await db.collection('users').insertOne({username:body.username,password:body.password}) 
+            const hashedPassword = await argon2.hash(body.password)
+            const result = await db.collection('users').insertOne({username:body.username,password:hashedPassword}) 
             res.status(200).json({message:'Account created'})
         }
         else{
-            res.status(401).json({message:'Username is alreday taken'})
+            res.status(409).json({message:'Username is alreday taken'})
         }
     } catch(error){
         return res.status(500).json({valid:false,message:'Server error during account creation'})
